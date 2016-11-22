c1323ce7ce6bbcf20893e4beb85688d142183301
Lv Tao <tao.a.lv@intel.com>
2016-11-22 09:18:12 +0000

    Update test_lrn.py to run lrn with random real data

--- theano/sandbox/mkl/tests/test_lrn.py
+++ theano/sandbox/mkl/tests/test_lrn.py
@@ -8,25 +8,23 @@ import theano.tensor.nnet
 import theano.tensor.nnet.lrn
 from theano.tensor.nnet.lrn import LRN
 
-x = T.ftensor4()
+import numpy
 
+x = T.ftensor4()
 y = LRN()(x)
 
-
+# forward
 theano.printing.pydotprint(y, outfile='lrn_fwd_before.png', var_with_name_simple=True)
-
 f = theano.function([x], y)
-
 theano.printing.pydotprint(f, outfile='lrn_fwd_after.png', var_with_name_simple=True)
 
-
-
+# backward
 z = T.grad(T.sum(y), [x])
-
 theano.printing.pydotprint(z, outfile='lrn_bwd_before.png', var_with_name_simple=True)
-
 f1 = theano.function([x], z)
-
 theano.printing.pydotprint(f1, outfile='lrn_bwd_after.png', var_with_name_simple=True)
 
-
+# random
+imval = numpy.random.rand(4, 2, 4, 4).astype(numpy.float32)
+f(imval)
+f1(imval)
